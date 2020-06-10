f526ec9f041703b33388546e0c93c7e018407ef2
dungvvfabbi <50563692+dungvvfabbi@users.noreply.github.com>
2020-06-10 16:16:26 +0700

    new

--- database/seeds/InitProductSeeder.php
+++ database/seeds/InitProductSeeder.php
@@ -11,57 +11,49 @@ class InitProductSeeder extends Seeder
      */
     public function run()
     {
-        $faker = Faker\Factory::create();
-
         for ($i=0; $i < 20; $i++) { 
-            $price1 = $faker->numberBetween(100000, 999999);
-            $price_sale1 = $faker->numberBetween(100000, $price1);
             DB::table('products')->insert([
                 'category_id' => 1,
-                'name' => $faker->unique()->text(15),
+                'name' => "Product Of Man",
                 'product_code' => 'PrdM'.$i,
-                'price' => $price1,
-                'price_sale' => $price_sale1,
-                'quantity' => $faker->numberBetween(10, 300),
-                'is_highlight' => $faker->numberBetween(0, 1),
-                'detail' => $faker->unique()->text(200),
-                'description' => $faker->unique()->text(500),
+                'price' => 999999,
+                'price_sale' => 888888,
+                'quantity' => 11,
+                'is_highlight' => 1,
+                'detail' => 'If multiple parameters are supplied then isset() will return TRUE only if all of the parameters are considered set. Evaluation goes from left to right and stops as soon as an unset variable is encountered.',
+                'description' => 'If multiple parameters are supplied then isset() will return TRUE only if all of the parameters are considered set. Evaluation goes from left to right and stops as soon as an unset variable is encountered.',
                 'created_at' => now(),
                 'updated_at' => now(),
             ]);
         }
 
         for ($i=0; $i < 20; $i++) { 
-            $price2 = $faker->numberBetween(100000, 999999);
-            $price_sale2 = $faker->numberBetween(100000, $price2);
             DB::table('products')->insert([
                 'category_id' => 2,
-                'name' => $faker->unique()->text(15),
-                'product_code' => 'PrdW'.$i,
-                'price' => $price2,
-                'price_sale' => $price_sale2,
-                'quantity' => $faker->numberBetween(10, 300),
-                'is_highlight' => $faker->numberBetween(0, 1),
-                'detail' => $faker->unique()->text(200),
-                'description' => $faker->unique()->text(500),
+                'name' => "Product Of Women",
+                'product_code' => 'PrdM'.$i,
+                'price' => 999999,
+                'price_sale' => 888888,
+                'quantity' => 11,
+                'is_highlight' => 1,
+                'detail' => 'If multiple parameters are supplied then isset() will return TRUE only if all of the parameters are considered set. Evaluation goes from left to right and stops as soon as an unset variable is encountered.',
+                'description' => 'If multiple parameters are supplied then isset() will return TRUE only if all of the parameters are considered set. Evaluation goes from left to right and stops as soon as an unset variable is encountered.',
                 'created_at' => now(),
                 'updated_at' => now(),
             ]);
         }
 
         for ($i=0; $i < 20; $i++) { 
-            $price3 = $faker->numberBetween(100000, 999999);
-            $price_sale3 = $faker->numberBetween(100000, $price3);
             DB::table('products')->insert([
                 'category_id' => 3,
-                'name' => $faker->unique()->text(15),
-                'product_code' => 'PrdK'.$i,
-                'price' => $price3,
-                'price_sale' => $price_sale3,
-                'quantity' => $faker->numberBetween(10, 300),
-                'is_highlight' => $faker->numberBetween(0, 1),
-                'detail' => $faker->unique()->text(200),
-                'description' => $faker->unique()->text(500),
+                'name' => "Product Of Kid",
+                'product_code' => 'PrdM'.$i,
+                'price' => 999999,
+                'price_sale' => 888888,
+                'quantity' => 11,
+                'is_highlight' => 1,
+                'detail' => 'If multiple parameters are supplied then isset() will return TRUE only if all of the parameters are considered set. Evaluation goes from left to right and stops as soon as an unset variable is encountered.',
+                'description' => 'If multiple parameters are supplied then isset() will return TRUE only if all of the parameters are considered set. Evaluation goes from left to right and stops as soon as an unset variable is encountered.',
                 'created_at' => now(),
                 'updated_at' => now(),
             ]);
